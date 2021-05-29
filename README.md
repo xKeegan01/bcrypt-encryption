c379d53494dc145f5438301d630c2199b935c1a3
Alexander Stratev <alexanderstratev@Alexanders-MacBook-Pro-2.local>
2021-05-28 19:13:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# BASIC AUTHENTICATION 
+
+This is a web application that uses basic bcrypt encryption.
+To download the necessary modules just use npm install. It will then download all the npm packages used in this app.
+
+[bcrypt](https://www.npmjs.com/package/bcrypt)
+
+Thank you!
---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const ejs = require('ejs');
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+const User =  mongoose.model('User', userSchema);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res) {
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
+    });
+});
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne ({email: username}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                });      
+            }
+        }
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Server running at port 3000...');
+});
